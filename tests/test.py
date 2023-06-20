2478c3201c29a4c7ea7578dd57b93d1091e9b89f
Tom Williams <tomjrw@gmail.com>
2023-06-20 17:27:03 +0100

    tests/test.py imports

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,7 @@
 
 # ---------------------------------------------------------------
 
-import sys
-import pathlib
-import importlib.util
-
-path = pathlib.Path("./src/xtuples/__init__.py").resolve()
-spec = importlib.util.spec_from_file_location(
-    "xtuples", str(path)
-    #
-)
-
-xtuples = importlib.util.module_from_spec(spec)
-sys.modules["xtuples"] = xtuples
-spec.loader.exec_module(xtuples)
-
-# ---------------------------------------------------------------
-
-print(xtuples.iTuple.range(3))
+import xtuples
+import xtuples.json
 
 # ---------------------------------------------------------------
